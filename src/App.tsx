1fe37a2eab3ed1ebda8dc27b9e6640b664ca43a1
Alan Cleyton <alancleyton67@gmail.com>
2022-11-13 20:30:13 -0300

    chore: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 function App() {
   return (
     <div className="App">
       <h2>Hello World!</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
