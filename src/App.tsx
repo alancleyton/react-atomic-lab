4e3dfacf185b08d129ef2291d064cb8e2a74c6d2
Alan Cleyton <alancleyton67@gmail.com>
2022-11-19 12:07:32 -0300

    style: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './styles/global.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World!</h2>
+      <h1 className="text-3xl font-bold underline bg-violet-400 max-w-sm">
+        Hello world!
+      </h1>
     </div>
   );
 }
